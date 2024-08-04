3a58a124c356a6e43d3a4144a69fada84375ab72
MohannadHamed <75036486+MohannadHamed@users.noreply.github.com>
2024-08-04 13:34:03 +0300

    Update bitmap_addr.sv

--- lib/display/bitmap_addr.sv
+++ lib/display/bitmap_addr.sv
@@ -3,7 +3,7 @@
 // Learn more at https://projectf.io
 
 `default_nettype none
-`timescale 1ns / 1ps
+//`timescale 1ns / 1ps
 
 // three-cycle address calculation
 // NB. doesn't handle coordinate wrapping using offsets
