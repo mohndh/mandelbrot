7d64b0c472c1afd4d7d98bdf9458a3b7c5605190
MohannadHamed <75036486+MohannadHamed@users.noreply.github.com>
2024-08-04 12:28:04 +0300

    Update mul.scala

--- lib/maths/mul.scala
+++ lib/maths/mul.scala
@@ -2,8 +2,6 @@
 // (C)2023 Will Green, Open source hardware released under the MIT License
 // Learn more at https://projectf.io/verilog-lib/
 
-`default_nettype none
-`timescale 1ns / 1ps
 
 module mul #(
     parameter WIDTH=8,  // width of numbers in bits (integer and fractional)
