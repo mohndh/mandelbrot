c526da1dd441a0b942c35c0f1cd54cc7fba5712b
mohndh <95147436+mohndh@users.noreply.github.com>
2024-08-13 22:35:38 +0300

    Update linebuffer_simple.sv

--- lib/display/linebuffer_simple.sv
+++ lib/display/linebuffer_simple.sv
@@ -3,7 +3,7 @@
 // Learn more at https://projectf.io
 
 `default_nettype none
-//`timescale 1ns / 1ps
+`timescale 1ns / 1ps
 
 module linebuffer_simple #(
     parameter DATAW=4,  // data width of each channel
