58e412b9cfc877feb8d75850b2b2e2c4a149ba59
mohndh <95147436+mohndh@users.noreply.github.com>
2024-08-13 22:25:15 +0300

    Update mul.sv

--- lib/maths/mul.sv
+++ lib/maths/mul.sv
@@ -1,37 +1,46 @@
+// Project F Library - Multiplication: Signed Fixed-Point with Gaussian Rounding
+// (C)2023 Will Green, Open source hardware released under the MIT License
+// Learn more at https://projectf.io/verilog-lib/
 
-module mul #() (
-    input wire logic clock,    // clock
-    input wire logic reset,    // reset
-    input wire logic io_start,  // start calculation
-    output     logic io_busy,   // calculation in progress
-    output     logic io_done,   // calculation is complete (high for one tick)
-    output     logic io_valid,  // result is valid
-    output     logic io_ovf,    // overflow
-    input wire logic signed [24:0] io_a,   // multiplier (factor)
-    input wire logic signed [24:0] io_b,   // mutiplicand (factor)
-    output     logic signed [24:0] io_valOut  // result value: product
+`default_nettype none
+`timescale 1ns / 1ps
+
+module mul #(
+    parameter WIDTH=8,  // width of numbers in bits (integer and fractional)
+    parameter FBITS=4   // fractional bits within WIDTH
+    ) (
+    input wire logic clk,    // clock
+    input wire logic rst,    // reset
+    input wire logic start,  // start calculation
+    output     logic busy,   // calculation in progress
+    output     logic done,   // calculation is complete (high for one tick)
+    output     logic valid,  // result is valid
+    output     logic ovf,    // overflow
+    input wire logic signed [WIDTH-1:0] a,   // multiplier (factor)
+    input wire logic signed [WIDTH-1:0] b,   // mutiplicand (factor)
+    output     logic signed [WIDTH-1:0] val  // result value: product
     );
 
     // for selecting result
-    localparam IBITS = 21;
-    localparam MSB = 49 - IBITS;
-    localparam LSB = 25 - IBITS;
+    localparam IBITS = WIDTH - FBITS;
+    localparam MSB = 2*WIDTH - IBITS - 1;
+    localparam LSB = WIDTH - IBITS;
 
     // for rounding
-    localparam HALF = {1'b1, {4-1{1'b0}}};
+    localparam HALF = {1'b1, {FBITS-1{1'b0}}};
 
     logic sig_diff;  // signs difference of inputs
-    logic signed [24:0] a1, b1;  // copy of inputs
-    logic signed [24:0] prod_t;  // unrounded, truncated product
-    logic signed [49:0] prod;  // full product
-    logic [3:0] rbits;          // rounding bits
+    logic signed [WIDTH-1:0] a1, b1;  // copy of inputs
+    logic signed [WIDTH-1:0] prod_t;  // unrounded, truncated product
+    logic signed [2*WIDTH-1:0] prod;  // full product
+    logic [FBITS-1:0] rbits;          // rounding bits
     logic round;  // rounding required
     logic even;   // even number
 
     // calculation state machine
     enum {IDLE, CALC, TRUNC, ROUND} state;
-    always_ff @(posedge clock) begin
-        io_done <= 0;
+    always_ff @(posedge clk) begin
+        done <= 0;
         case (state)
             CALC: begin
                 state <= TRUNC;
@@ -41,47 +50,55 @@ module mul #() (
                 // need to check for overflow (need to look at MSB)
                 state <= ROUND;
                 prod_t <= prod[MSB:LSB];
-                rbits  <= prod[4-1:0];
-                round  <= prod[4-1+:1];
-                even  <= ~prod[4+:1];
+                rbits  <= prod[FBITS-1:0];
+                round  <= prod[FBITS-1+:1];
+                even  <= ~prod[FBITS+:1];
             end
             ROUND: begin  // round half to even
                 state <= IDLE;
-                io_busy <= 0;
-                io_done <= 1;
+                busy <= 0;
+                done <= 1;
 
                 // Gaussian rounding
-                io_valOut <= (round && !(even && rbits == HALF)) ? prod_t + 1 : prod_t;
+                val <= (round && !(even && rbits == HALF)) ? prod_t + 1 : prod_t;
 
                 // overflow
-                if (sig_diff == prod_t[24+:1] &&  // compare input and answer sign
-                    (prod[49:MSB+1] == '0 || prod[49:MSB+1] == '1)  // overflow bits
+                if (sig_diff == prod_t[WIDTH-1+:1] &&  // compare input and answer sign
+                    (prod[2*WIDTH-1:MSB+1] == '0 || prod[2*WIDTH-1:MSB+1] == '1)  // overflow bits
                 ) begin
-                    io_valid <= 1;
-                    io_ovf <= 0;
+                    valid <= 1;
+                    ovf <= 0;
                 end else begin
-                    io_valid <= 0;
-                    io_ovf <= 1;
+                    valid <= 0;
+                    ovf <= 1;
                 end
             end
             default: begin
-                if (io_start) begin
+                if (start) begin
                     state <= CALC;
                     a1 <= a;  // register input a
                     b1 <= b;  // register input b
-                    sig_diff <= (a[24+:1] ^ b[24+:1]);  // register input sign difference
-                    io_busy <= 1;
-                    io_ovf <= 0;
+                    sig_diff <= (a[WIDTH-1+:1] ^ b[WIDTH-1+:1]);  // register input sign difference
+                    busy <= 1;
+                    ovf <= 0;
                 end
             end
         endcase
-        if (reset) begin
+        if (rst) begin
             state <= IDLE;
-            io_busy <= 0;
-            io_done <= 0;
-            io_valid <= 0;
-            io_ovf <= 0;
-            io_val <= 0;
+            busy <= 0;
+            done <= 0;
+            valid <= 0;
+            ovf <= 0;
+            val <= 0;
         end
     end
+
+    // generate waveform file with cocotb
+    `ifdef COCOTB_SIM
+    initial begin
+        $dumpfile($sformatf("%m.vcd"));
+        $dumpvars;
+    end
+    `endif
 endmodule
