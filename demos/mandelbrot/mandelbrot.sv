53a649be244b09fd5c5b02fd14b387b49c71dc37
mohndh <95147436+mohndh@users.noreply.github.com>
2024-08-17 20:13:19 +0300

    Update mandelbrot.sv

--- demos/mandelbrot/mandelbrot.sv
+++ demos/mandelbrot/mandelbrot.sv
@@ -19,98 +19,170 @@ module mandelbrot #(
     output      logic done  // calculation complete (high for one tick)
     );
 
-    // intermediate values
-    logic signed [25-1:0] x0, y0, x2, y2, x, y;
-
-    // fixed-point multiplication module
-    logic signed [25-1:0] mul_a, mul_b, mul_val, mul_val_p;
-    logic mul_start, mul_done;
-    mul #(.WIDTH(25), .FBITS(25 - 4)) mul_inst (
-        .clk,
-        .rst,
-        .start(mul_start),
-        /* verilator lint_off PINCONNECTEMPTY */
-        .busy(),
-        /* verilator lint_on PINCONNECTEMPTY */
-        .done(mul_done),
-        /* verilator lint_off PINCONNECTEMPTY */
-        .valid(),
-        .ovf(),
-        /* verilator lint_on PINCONNECTEMPTY */
-        .a(mul_a),
-        .b(mul_b),
-        .val(mul_val)
-    );
-
-    /* verilator lint_off UNUSED */
-    logic signed [25-1:0] xt, xy2;  // temporaries
-    /* verilator lint_on UNUSED */
-
-    enum {IDLE, STEP1, STEP2, STEP2A, STEP3, STEP4} state;
-    always_ff @(posedge clk) begin
-        done <= 0;
-        case (state)
-            STEP1: begin
-                if ((xy2[25-1-:4] <= 4) && (iter < 255)) begin
-                    state <= STEP2;
-                    mul_a <= x;
-                    mul_b <= y;
-                    mul_start <= 1;
-                end else begin
-                    state <= IDLE;
-                    calculating <= 0;
-                    done <= 1;
-                end
-            end
-            STEP2: begin
-                mul_start <= 0;
-                if (mul_done) begin  // register results
-                    state <= STEP2A;
-                    mul_val_p <= mul_val;
-                    xt <= x2 - y2 + x0;
-                end
-            end
-            STEP2A: begin
-                state <= STEP3;
-                y <= 2 * mul_val_p + y0;
-                x <= xt;
-                mul_a <= xt;
-                mul_b <= xt;
-                mul_start <= 1;
-            end
-            STEP3: begin
-                mul_start <= 0;
-                if (mul_done) begin
-                    state <= STEP4;
-                    x2 <= mul_val;
-                    mul_a <= y;
-                    mul_b <= y;
-                    mul_start <= 1;
-                end
-            end
-            STEP4: begin
-                mul_start <= 0;
-                if (mul_done) begin
-                    state <= STEP1;
-                    y2 <= mul_val;
-                    xy2 <= x2 + mul_val;
-                    iter <= iter + 1;
-                end
-            end
-            default: if (start) begin  // IDLE
-                state <= STEP1;
-                calculating <= 1;
-                x0 <= re;  // register coordinates
-                y0 <= im;
-                x <= 0;
-                y <= 0;
-                x2 <= 0;
-                y2 <= 0;
-                xy2 <= 0;
-                xt <= 0;
-                iter <= 0;
-            end
-        endcase
-        if (rst) state <= IDLE;
+  wire        _mulModule_io_done;
+  wire [24:0] _mulModule_io_valOut;
+  reg  [24:0] x0;
+  reg  [24:0] y0;
+  reg  [24:0] x2;
+  reg  [24:0] y2;
+  reg  [24:0] x;
+  reg  [24:0] y;
+  reg  [24:0] mulA;
+  reg  [24:0] mulB;
+  reg  [24:0] mulValP;
+  reg         mulStart;
+  reg  [24:0] xt;
+  reg  [24:0] xy2;
+  reg  [2:0]  state;
+  reg  [7:0]  iter;
+  reg         calculating;
+  reg         done;
+  always @(posedge clk) begin
+    automatic logic             _GEN;
+    automatic logic             _GEN_0;
+    automatic logic             _GEN_1;
+    automatic logic             _GEN_2;
+    automatic logic             _GEN_3;
+    automatic logic             _GEN_4;
+    automatic logic             _GEN_5;
+    automatic logic             _GEN_6;
+    automatic logic             _GEN_7;
+    automatic logic             _GEN_8;
+    automatic logic [24:0]      _GEN_9;
+    automatic logic [24:0]      _GEN_10;
+    automatic logic [7:0][24:0] _GEN_11;
+    automatic logic [7:0][24:0] _GEN_12;
+    _GEN = state == 3'h0;
+    _GEN_0 = state == 3'h1;
+    _GEN_1 = $signed(xy2[24:21]) < 4'sh5 & iter != 8'hFF;
+    _GEN_2 = state == 3'h2;
+    _GEN_3 = _GEN_2 & _mulModule_io_done;
+    _GEN_4 = state == 3'h3;
+    _GEN_5 = state == 3'h4;
+    _GEN_6 = state == 3'h5;
+    _GEN_7 = _GEN_6 & _mulModule_io_done;
+    _GEN_8 = _GEN_0 | _GEN_2 | _GEN_4 | _GEN_5 | ~_GEN_7;
+    _GEN_9 = _GEN_8 ? y2 : _mulModule_io_valOut;
+    _GEN_10 = _GEN_8 ? xy2 : x2 + _mulModule_io_valOut;
+    if (_GEN & start) begin
+      x0 <= re;
+      y0 <= im;
     end
+    if (_GEN) begin
+      if (start) begin
+        x2 <= 25'h0;
+        x <= 25'h0;
+        y <= 25'h0;
+        xt <= 25'h0;
+      end
+    end
+    else begin
+      automatic logic _GEN_13;
+      _GEN_13 = _GEN_5 & _mulModule_io_done;
+      if (_GEN_0 | _GEN_2 | _GEN_4 | ~_GEN_13) begin
+      end
+      else
+        x2 <= _mulModule_io_valOut;
+      if (_GEN_0 | _GEN_2 | ~_GEN_4) begin
+      end
+      else begin
+        x <= xt;
+        y <= {mulValP[23:0], 1'h0} + y0;
+      end
+      if (_GEN_0) begin
+        if (_GEN_1) begin
+          mulA <= x;
+          mulB <= y;
+        end
+        mulStart <= _GEN_1 | mulStart;
+      end
+      else begin
+        if (~_GEN_2) begin
+          if (_GEN_4) begin
+            mulA <= xt;
+            mulB <= xt;
+          end
+          else if (_GEN_13) begin
+            mulA <= y;
+            mulB <= y;
+          end
+        end
+        mulStart <=
+          ~_GEN_2 & (_GEN_4 | (_GEN_5 ? _mulModule_io_done : ~_GEN_6 & mulStart));
+      end
+      if (_GEN_0 | ~_GEN_3) begin
+      end
+      else
+        xt <= x2 - y2 + x0;
+    end
+    _GEN_11 =
+      {{_GEN_9}, {_GEN_9}, {_GEN_9}, {y2}, {y2}, {y2}, {y2}, {start ? 25'h0 : y2}};
+    y2 <= _GEN_11[state];
+    if (_GEN | _GEN_0 | ~_GEN_3) begin
+    end
+    else
+      mulValP <= _mulModule_io_valOut;
+    _GEN_12 =
+      {{_GEN_10},
+       {_GEN_10},
+       {_GEN_10},
+       {xy2},
+       {xy2},
+       {xy2},
+       {xy2},
+       {start ? 25'h0 : xy2}};
+    xy2 <= _GEN_12[state];
+    if (rst) begin
+      state <= 3'h0;
+      iter <= 8'h0;
+      calculating <= 1'h0;
+      done <= 1'h0;
+    end
+    else begin
+      automatic logic [2:0]      _GEN_14;
+      automatic logic [7:0]      _GEN_15;
+      automatic logic [7:0][2:0] _GEN_16;
+      automatic logic [7:0][7:0] _GEN_17;
+      _GEN_14 = _GEN_7 ? 3'h1 : state;
+      _GEN_15 = _GEN_8 ? iter : iter + 8'h1;
+      _GEN_16 =
+        {{_GEN_14},
+         {_GEN_14},
+         {_GEN_14},
+         {_mulModule_io_done ? 3'h5 : state},
+         {3'h4},
+         {_mulModule_io_done ? 3'h3 : state},
+         {{1'h0, _GEN_1, 1'h0}},
+         {start ? 3'h1 : state}};
+      state <= _GEN_16[state];
+      _GEN_17 =
+        {{_GEN_15},
+         {_GEN_15},
+         {_GEN_15},
+         {iter},
+         {iter},
+         {iter},
+         {iter},
+         {start ? 8'h0 : iter}};
+      iter <= _GEN_17[state];
+      if (_GEN)
+        calculating <= start | calculating;
+      else
+        calculating <= (~_GEN_0 | _GEN_1) & calculating;
+      if (_GEN | ~_GEN_0) begin
+      end
+      else
+        done <= ~_GEN_1;
+    end
+  end // always @(posedge)
+  mul mulModule (
+    .clk     (clk),
+    .rst     (rst),
+    .start  (mulStart),
+    .done   (_mulModule_io_done),
+    .ia      (mulA),
+    .b      (mulB),
+    .val (_mulModule_io_valOut)
+  );
 endmodule
+
